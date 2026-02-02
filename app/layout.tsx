c781cb75259c9544e8b967795ab46083bc056c8b
mark-cooper506 <markcooper506@gmail.com>
2026-02-02 16:10:52 +0500

    SEO setup + metadata fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
-// app/layout.tsx
-"use client";
-
 import Footer from "./common/footer";
 import Navbar from "./common/navbar";
-import './globals.css'; // ✅ Global CSS
+import "./globals.css";
 import WhatsAppFloat from "./WhatsAppFloat/page";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: "Firestick 4K IPTV Packages | Best Streaming Deals",
+  description:
+    "Get affordable Firestick 4K IPTV packages with live TV, movies, and sports. Easy setup and fast streaming.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-      <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>
-        <Navbar />  {/* Navbar globally */}
-        {children}  {/* Pages content */}
-          <Footer />
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
         <WhatsAppFloat />
       </body>
     </html>
